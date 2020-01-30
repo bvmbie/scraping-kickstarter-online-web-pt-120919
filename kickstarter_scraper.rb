d7f7827a7dcebb04c2f3bacf7ce6ea635de04061
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-30 10:51:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,7 +15,12 @@ def create_project_hash
   
   projects = {}
   
-  kickstarter
+  kickstarter.css("li.project.grid_4").each 
+  do |project|
+    projects[project] = {}
+  end
+  
+  projects
 end
 
 create_project_hash
